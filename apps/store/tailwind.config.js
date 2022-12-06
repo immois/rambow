6382cd48ba49056b130eca55038e1ce95b57e110
Rolando Machuca Valverde <rmoimv@gmail.com>
2022-12-06 16:09:58 -0500

    chore(store): tailwindscss setup

--- apps/store/tailwind.config.js
+++ apps/store/tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("@rambow/ui/tailwind")],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@rambow/ui/**/*.js'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@rambow/ui/**/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
