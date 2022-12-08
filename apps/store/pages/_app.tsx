5fa7f50d91adcba0fd1089f39b2889b91338ad54
Rolando Machuca Valverde <rmoimv@gmail.com>
2022-12-07 23:59:07 -0500

    feat(store): add app-layout and global css

--- apps/store/pages/_app.tsx
+++ apps/store/pages/_app.tsx
@@ -1,10 +1,12 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import AppLayout from '@layouts/app-layout';
+import '@rambow/ui/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    <>
+    <AppLayout>
       <Component {...pageProps} />
-    </>
+    </AppLayout>
   )
 }
