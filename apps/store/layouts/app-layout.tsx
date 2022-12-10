02ea0384ab2cfffe0b7c0c4961e9644336b7c8ce
Rolando Machuca Valverde <rmoimv@gmail.com>
2022-12-09 19:24:49 -0500

    feat(store):  add SidebarMenu in app-layout

--- apps/store/layouts/app-layout.tsx
+++ apps/store/layouts/app-layout.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
 import { Header } from "@components/header"
 import { Children } from "@interfaces/children"
+import SidebarMenu from "@components/sidebar/sidebar-menu"
 
 const AppLayout = ({ children }: Children) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const toogleSideMenu = () => setIsActive(!isActive)
+  const closeSidebarMenu = () => isActive && setIsActive(false)
 
   return (
     <>
-      <Header />
-      {children}
+      <SidebarMenu />
+
+      <div onClick={closeSidebarMenu} className={`w-screen h-screen bg-slate-200 relative z-10 duration-300 ${isActive ?
+        'scale-x-100 scale-y-90 translate-x-[58%] rounded-2xl' : ''}`}
+      >
+        <Header clickMenu={toogleSideMenu} />
+        <main>
+          {children}
+        </main>
+      </div>
     </>
   )
 }
